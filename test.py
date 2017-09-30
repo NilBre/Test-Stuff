eb8ff77488c801acdcfb0e9ea4d0e5792e1beb3e
Nils Breer <nilsbreer@icloud.com>
2017-09-30 12:05:45 +0200

    better legend implemented

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x=np.linspace(0,2*np.pi,100)
-plt.plot(x, np.sin(x),"r-", label=r"$\sin(x)$")
+plt.plot(x, np.sin(x),"r-", label=r"$sin(x)$")
 plt.legend()
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
